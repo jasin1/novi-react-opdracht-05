cdc117af8ec536b28395272595a66f95da5d8f9b
jasin1 <jasin.tairaidrissi@gmail.com>
2023-12-06 13:27:16 +0100

    Opdracht 1 basics af

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,72 @@
 import './App.css';
+import axios from "axios";
+import {useState} from "react";
+import worldMap from './assets/world_map.png'
+import regionColor from "./helpers/regionColor.js";
+
+
+// endpoint for all https://restcountries.com/v3.1/all
 
 function App() {
+    const [countries, setCountries] = useState([]);
+    const [error, setError] = useState('');
+
+    async function fetchCountries() {
+        setError('')
+        try {
+            const result = await
+                axios.get('https://restcountries.com/v3.1/all');
+            console.log(result.data[0]);
+            setCountries(result.data);
+
+        } catch (e) {
+            console.error(e);
+            setError('Het ophalen van de data is mislukt. Probeer het opnieuw');
+        }
+    }
+
+
+
 
     return (
         <>
-            Maak je applicatie hier!
+            <main>
+                <section>
+                <header>
+                    <div className="worldMap-container">
+                        <img src={worldMap} alt=""/>
+                    </div>
+                </header>
+                <h1>World Regions</h1>
+
+
+                    <article>
+                        <div className="btn-wrapper">
+                        {error && <p className="error">{error}</p>}
+                        <button type="button" onClick={fetchCountries}>click me</button>
+                        </div>
+                    </article>
+                    <article className="countries-wrapper">
+                        {countries.map((country, index) => (
+                            <ul>
+                                <li key={country.name?.common}>
+                                    <div className="country-container">
+                                        <div className="flag-wrapper">
+                                            <img src={country.flags?.svg} alt="country flag"/>
+                                        </div>
+                                        <div className="country-info">
+                                            <span className={regionColor(country.region)}>{country.name?.official}</span>
+                                            <p>Region: {country.region}</p>
+                                            <p>Has a population of {country.population} people</p>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+
+                        ))}
+                    </article>
+                </section>
+            </main>
         </>
     )
 }
---
+++ src/helpers/regionColor.js
@@ -0,0 +1,28 @@
+function regionColor(region) {
+    let className = '';
+
+    switch (region) {
+        case 'Africa':
+            className = 'blue-text'
+            break;
+        case 'Americas':
+            className = 'green-text'
+            break;
+        case 'Asia':
+            className = 'red-text'
+            break;
+        case 'Europe':
+            className = 'yellow-text'
+            break;
+        case 'Oceania':
+            className = 'purple-text'
+            break;
+
+    }
+
+    return className
+
+}
+
+export default regionColor;
+
